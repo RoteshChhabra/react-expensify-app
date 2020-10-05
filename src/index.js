1903d99362894255e16d9ddc661e66c3dfac9ef8
Rotesh Chhabra <batman@Roteshs-MacBook-Air.local>
2020-10-05 20:28:15 +0530

    remove dummy expense data

--- src/index.js
+++ src/index.js
@@ -17,14 +17,6 @@ import './styles/styles.scss';
 const store = configureStore();
 
 
-store.dispatch(addExpense({description:'Water bill',amount:1200}));
-store.dispatch(addExpense({description:'Gas bill',amount:1000}));
-store.dispatch(addExpense({description:'Rent',amount:5000}));
-//store.dispatch(setTextFilter('Water'));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses,state.filters);
-console.log(visibleExpenses);
 
 const jsx= (
   <Provider store= {store}>
